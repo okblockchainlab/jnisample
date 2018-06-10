411da0d8b481650b4c23d21d837f710682199935
zhongqiuwood <zhongqiuwood@gmail.com>
2018-06-10 11:01:56 +0800

    use.cpp

--- run.darwin.sh
+++ run.darwin.sh
@@ -7,6 +7,12 @@ javah -jni HelloWorld
 g++ -gdwarf-4 \
     -I$JAVA_HOME/include \
     -I$JAVA_HOME/include/darwin \
-    -fPIC -shared -o libHelloWorld.dylib HelloWorld.c
+    -fPIC -shared -o libHelloWorld.dylib HelloWorld.cpp
 
 java -Djava.library.path='.' HelloWorld
+
+echo "=====================Dump libHelloWorld.dylib info===================="
+
+otool -L libHelloWorld.dylib
+
+nm libHelloWorld.dylib
--- run.linux.sh
+++ run.linux.sh
@@ -7,6 +7,11 @@ javah -jni HelloWorld
 g++ -gdwarf-4 \
     -I$JAVA_HOME/include \
     -I$JAVA_HOME/include/darwin \
-    -fPIC -shared -o libHelloWorld.so HelloWorld.c
+    -fPIC -shared -o libHelloWorld.so HelloWorld.cpp
 
 java -Djava.library.path='.' HelloWorld
+
+echo "=====================Dump libHelloWorld.so info===================="
+
+ldd libHelloWorld.so
+nm libHelloWorld.so
