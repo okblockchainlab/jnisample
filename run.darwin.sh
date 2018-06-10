5af208778956ad2c95955a1b7d99b8eae54a25d6
zhongqiuwood <zhongqiuwood@gmail.com>
2018-06-10 21:40:28 +0800

    darwin dylib

--- run.darwin.sh
+++ run.darwin.sh
@@ -7,7 +7,7 @@ javah -jni HelloWorld
 g++ -gdwarf-4 \
     -I$JAVA_HOME/include \
     -I$JAVA_HOME/include/darwin \
-    -fPIC -shared -o libHelloWorld.dylib HelloWorld.cpp
+    -fPIC -dynamiclib -o libHelloWorld.dylib HelloWorld.cpp
 
 java -Djava.library.path='.' HelloWorld
 
